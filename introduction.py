002dd6c92b3bc2c9388c09181068745488ce35c1
fela101 <ngangafelix25@gmail.com>
2023-07-12 00:23:23 +0300

    introducton to python

--- introduction.py
+++ introduction.py
@@ -1,1 +1,23 @@
-print("me only")
+  #This is a comment.
+   #print("hello world") 
+#x= "awesome"
+#def myfunc():
+ #  print("python is " + x)
+#myfunc()
+#print("python is " + x)
+#def myfunc():
+ #  global x
+  # x = "fantastic"
+#myfunc()
+#print("python is " + x)
+#x,y = (5,10)
+#sum = (x+y)
+#print(sum)
+import random
+print(random.randrange(1, 10))
+
+
+
+
+
+   
