9ea5d49913231a4f1379dfb1f9c214d034468fdf
fela101 <ngangafelix25@gmail.com>
2023-07-14 14:15:17 +0300

    updated

--- function.py
+++ function.py
@@ -7,3 +7,10 @@ def my_name():
     name = input ("what is your name: ")
     print("my name is "+ name)
 my_name()
+x = float(1)
+w = float(4.2)
+print(x,w) 
+a= """ Lorem ispsum dolor sit amet,consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labour et dolore magna aliqua"""#multiline string
+print([a])
+b = "hello world"
+print = ([b])
