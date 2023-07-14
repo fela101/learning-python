c5599a6607cc9421645069e15dd41e2eab1d64a8
fela101 <ngangafelix25@gmail.com>
2023-07-14 14:12:35 +0300

    slicing strings haven't understood it yet

--- loops.py
+++ loops.py
@@ -2,4 +2,37 @@ for x in "banana":
  print (x)
  a = "hello world"
 print(len(a))
+# to check string
+txt = "the best things in life are free!"
+print("free"in txt)
+#using if statement
+txt = "the best things in life are free!"
+if "free" in txt:
+ print("yes,'free' is present.")
+ #to check if not
+ txt = "the best things in life are free!"
+ print("expensive" not in txt)
+ # using it in an if statement
+ txt = "The best things in life are free"
+ if "expensive" not in txt:
+  print ("no, 'expensive' is not present")
+  #slicing
+  b = "hello world"
+  print(b[2:5])
+  #slicing from start
+  c = "hello world"
+  print(b[:5])
+  #slicing to the end
+  b = "hello world"
+  print(b[2:])
+  #negative indexing
+  b = "hello world"
+  print(b[-5:-2])
+  
+
+
+
+
+
+
 
